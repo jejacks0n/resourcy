4d7c66627350f9072ed5b9c8e7b1a8d962850af0
Jeremy Jackson <jejacks0n@ishifishipro.(none)>
2012-09-11 18:51:02 -0600

    build distro

---
+++ distro/jquery.resourcy.js
@@ -0,0 +1,261 @@
+(function() {
+  var actions, addCallback, createResource, descriptions, determineCallback, handleRequest, methods, parseUrl, resources,
+    _this = this;
+
+  resources = {};
+
+  methods = ['get', 'put', 'post', 'delete'];
+
+  actions = ['index', 'create', 'new', 'edit', 'show', 'update', 'delete'];
+
+  descriptions = {
+    plural: [['get', ''], ['post', ''], ['get', '/new'], ['get', '/:id/edit'], ['get', '/:id'], ['put', '/:id'], ['delete', '/:id']],
+    singular: [null, ['post', ''], ['get', '/new'], ['get', '/edit'], ['get', ''], ['put', ''], ['delete', '']]
+  };
+
+  parseUrl = function(url) {
+    var path, _ref;
+    url = url.match(/^((http[s]?|ftp):\/\/)?(((.+)@)?([^:\/\?#\s]+)(:(\d+))?)?(\/?[^\?#\.]+)?(\.([^\?#]+))?(\?([^#]*))?(#(.*))?$/i) || [];
+    path = ((_ref = url[9]) != null ? _ref.match(/(\/.*)\/+(\w+)$/i) : void 0) || [];
+    return {
+      scheme: url[2],
+      credentials: url[5],
+      host: url[6],
+      port: url[8],
+      path: url[9],
+      action: path[2] || '',
+      format: url[11],
+      query: url[13],
+      hash: url[15]
+    };
+  };
+
+  createResource = function(path, singular) {
+    if (singular == null) {
+      singular = false;
+    }
+    if (resources[path]) {
+      return resources[path];
+    }
+    return resources[path] = {
+      path: new RegExp("^" + ((path[0] === '/' ? path : "/" + path).replace(/:\w+/ig, '(\\w+)')) + "/?(\\w+)?/?(\\w+)?/?($|\\?|\\.|#)", 'i'),
+      pathvars: (path.match(/:(\w+)/ig) || []).join('|').replace(/:/g, '').split('|'),
+      actions: {},
+      singular: singular,
+      name: path.substr(path.lastIndexOf('/') + 1),
+      add: function(action, callback) {
+        var method, object, _ref;
+        object = {};
+        if (typeof action === 'string') {
+          object[action] = callback;
+        } else {
+          object = action;
+        }
+        for (action in object) {
+          callback = object[action];
+          _ref = action.split(':'), method = _ref[0], action = _ref[1];
+          addCallback.call(this, method, action, callback);
+        }
+        return this;
+      },
+      remove: function(action) {
+        var method, _ref;
+        _ref = action.split(':'), method = _ref[0], action = _ref[1];
+        if (action) {
+          delete this.actions[method][action];
+        } else {
+          delete this.actions[method];
+        }
+        return this;
+      },
+      removeAll: function() {
+        this.actions = {};
+        return this;
+      },
+      describe: function() {
+        var action, desc, i, method, routes, start, _i, _j, _len, _ref, _ref1;
+        routes = [];
+        for (_i = 0, _len = methods.length; _i < _len; _i++) {
+          method = methods[_i];
+          for (action in this.actions[method]) {
+            routes.push("" + path + "/" + action + " " + (method.toUpperCase()) + " => " + this.name + "#" + action);
+          }
+        }
+        _ref = this.singular ? [1, descriptions.singular] : [0, descriptions.plural], start = _ref[0], desc = _ref[1];
+        for (i = _j = start, _ref1 = actions.length - 1; start <= _ref1 ? _j <= _ref1 : _j >= _ref1; i = start <= _ref1 ? ++_j : --_j) {
+          if (this.actions[actions[i]]) {
+            routes.push("" + path + desc[i][1] + " " + (desc[i][0].toUpperCase()) + " => " + this.name + "#" + actions[i]);
+          }
+        }
+        return routes;
+      }
+    };
+  };
+
+  addCallback = function(method, action, callback) {
+    var errorMsg, _base;
+    errorMsg = "The " + ([method, action].join(':').replace(/:$/, '')) + " action already exists on the '" + this.name + "' resource. Try removing it first.";
+    if (action) {
+      (_base = this.actions)[method] || (_base[method] = {});
+      if (this.actions[method][action]) {
+        throw errorMsg;
+      }
+      return this.actions[method][action] = callback;
+    } else {
+      if (this.actions[method]) {
+        throw errorMsg;
+      }
+      if (this.singular && method === 'index') {
+        throw "Adding index to '" + this.name + "' isn't possible (singular resource).";
+      }
+      return this.actions[method] = callback;
+    }
+  };
+
+  handleRequest = function(method, url, options, original, optionsHandler) {
+    var action, callback, index, key, matches, path, pathvar, proceed, proceeded, resource, result, urlParts, vars, _i, _len, _ref, _ref1;
+    method = method.toLowerCase();
+    _ref = urlParts = parseUrl(url), path = _ref.path, action = _ref.action;
+    proceeded = false;
+    proceed = function(opts) {
+      proceeded = true;
+      return original(url, optionsHandler(options || {}, opts || {}));
+    };
+    for (key in resources) {
+      resource = resources[key];
+      if (!(matches = path.match(resource.path))) {
+        continue;
+      }
+      if (callback = determineCallback(resource, action, method, matches[matches.length - 2], matches[matches.length - 3])) {
+        vars = {};
+        _ref1 = resource.pathvars;
+        for (index = _i = 0, _len = _ref1.length; _i < _len; index = ++_i) {
+          pathvar = _ref1[index];
+          vars[pathvar] = matches[index + 1];
+        }
+        result = callback(proceed, vars, urlParts);
+      }
+      if (result !== false && !proceeded) {
+        return proceed(result);
+      }
+      return;
+    }
+    return original(url, options);
+  };
+
+  determineCallback = function(resource, action, method, matchAction, matchIdOrAction) {
+    var _ref, _ref1, _ref2, _ref3;
+    switch (method) {
+      case 'get':
+        if ((_ref = resource.actions.get) != null ? _ref[action] : void 0) {
+          return resource.actions.get[action];
+        }
+        switch ((matchIdOrAction ? action : '')) {
+          case '':
+            return (resource.singular ? resource.actions.show : resource.actions.index);
+          case 'new':
+            return resource.actions["new"];
+          case 'edit':
+            return resource.actions.edit;
+          default:
+            if (!matchAction) {
+              return resource.actions.show;
+            }
+        }
+        break;
+      case 'put':
+        return ((_ref1 = resource.actions.put) != null ? _ref1[action] : void 0) || (!matchAction ? resource.actions.update : void 0);
+      case 'post':
+        return ((_ref2 = resource.actions.post) != null ? _ref2[action] : void 0) || (!matchIdOrAction ? resource.actions.create : void 0);
+      case 'delete':
+        return ((_ref3 = resource.actions["delete"]) != null ? _ref3[action] : void 0) || (!matchAction ? resource.actions.destroy : void 0);
+    }
+  };
+
+  this.Resourcy = {
+    removeAll: function() {
+      return resources = {};
+    },
+    handleRequest: handleRequest,
+    noConflict: Resourcy.noConflict || function() {
+      return delete Resourcy;
+    },
+    resources: function(path, actions) {
+      if (actions == null) {
+        actions = {};
+      }
+      return createResource(path).add(actions);
+    },
+    resource: function(path, actions) {
+      if (actions == null) {
+        actions = {};
+      }
+      return createResource(path, true).add(actions);
+    },
+    routes: function() {
+      var path, resource, routes;
+      routes = {};
+      for (path in resources) {
+        resource = resources[path];
+        routes[resource.name] = resource.describe();
+      }
+      return routes;
+    }
+  };
+
+}).call(this);
+(function() {
+  var handleRequest, optionsHandler, original;
+
+  original = jQuery.ajax;
+
+  jQuery.extend(jQuery, {
+    originalAjax: original,
+    resources: Resourcy.resources,
+    resource: Resourcy.resource,
+    routes: Resourcy.routes
+  });
+
+  handleRequest = Resourcy.handleRequest;
+
+  Resourcy.noConflict();
+
+  optionsHandler = function(opts1, opts2) {
+    var c1, c2, method, options, _i, _len, _ref;
+    options = jQuery.extend(true, {}, opts1, opts2);
+    _ref = ['beforeSend', 'error', 'dataFilter', 'success', 'complete'];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      method = _ref[_i];
+      if (opts1[method] && opts2[method]) {
+        c1 = opts1[method];
+        c2 = opts2[method];
+        options[method] = function() {
+          c1.apply(window, arguments);
+          return c2.apply(window, arguments);
+        };
+      }
+    }
+    return options;
+  };
+
+  jQuery.ajax = function(url, options) {
+    var data, method, _i, _len, _ref;
+    if (options == null) {
+      options = {};
+    }
+    if (typeof url === 'object') {
+      options = url;
+      url = options.url;
+    }
+    _ref = options.data || [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      data = _ref[_i];
+      if (data.name === '_method') {
+        method = data.value;
+        break;
+      }
+    }
+    return handleRequest(method || options.type || 'get', url, options, jQuery.originalAjax, optionsHandler);
+  };
+
+}).call(this);
---
+++ distro/resourcy.js
@@ -0,0 +1,206 @@
+(function() {
+  var actions, addCallback, createResource, descriptions, determineCallback, handleRequest, methods, parseUrl, resources,
+    _this = this;
+
+  resources = {};
+
+  methods = ['get', 'put', 'post', 'delete'];
+
+  actions = ['index', 'create', 'new', 'edit', 'show', 'update', 'delete'];
+
+  descriptions = {
+    plural: [['get', ''], ['post', ''], ['get', '/new'], ['get', '/:id/edit'], ['get', '/:id'], ['put', '/:id'], ['delete', '/:id']],
+    singular: [null, ['post', ''], ['get', '/new'], ['get', '/edit'], ['get', ''], ['put', ''], ['delete', '']]
+  };
+
+  parseUrl = function(url) {
+    var path, _ref;
+    url = url.match(/^((http[s]?|ftp):\/\/)?(((.+)@)?([^:\/\?#\s]+)(:(\d+))?)?(\/?[^\?#\.]+)?(\.([^\?#]+))?(\?([^#]*))?(#(.*))?$/i) || [];
+    path = ((_ref = url[9]) != null ? _ref.match(/(\/.*)\/+(\w+)$/i) : void 0) || [];
+    return {
+      scheme: url[2],
+      credentials: url[5],
+      host: url[6],
+      port: url[8],
+      path: url[9],
+      action: path[2] || '',
+      format: url[11],
+      query: url[13],
+      hash: url[15]
+    };
+  };
+
+  createResource = function(path, singular) {
+    if (singular == null) {
+      singular = false;
+    }
+    if (resources[path]) {
+      return resources[path];
+    }
+    return resources[path] = {
+      path: new RegExp("^" + ((path[0] === '/' ? path : "/" + path).replace(/:\w+/ig, '(\\w+)')) + "/?(\\w+)?/?(\\w+)?/?($|\\?|\\.|#)", 'i'),
+      pathvars: (path.match(/:(\w+)/ig) || []).join('|').replace(/:/g, '').split('|'),
+      actions: {},
+      singular: singular,
+      name: path.substr(path.lastIndexOf('/') + 1),
+      add: function(action, callback) {
+        var method, object, _ref;
+        object = {};
+        if (typeof action === 'string') {
+          object[action] = callback;
+        } else {
+          object = action;
+        }
+        for (action in object) {
+          callback = object[action];
+          _ref = action.split(':'), method = _ref[0], action = _ref[1];
+          addCallback.call(this, method, action, callback);
+        }
+        return this;
+      },
+      remove: function(action) {
+        var method, _ref;
+        _ref = action.split(':'), method = _ref[0], action = _ref[1];
+        if (action) {
+          delete this.actions[method][action];
+        } else {
+          delete this.actions[method];
+        }
+        return this;
+      },
+      removeAll: function() {
+        this.actions = {};
+        return this;
+      },
+      describe: function() {
+        var action, desc, i, method, routes, start, _i, _j, _len, _ref, _ref1;
+        routes = [];
+        for (_i = 0, _len = methods.length; _i < _len; _i++) {
+          method = methods[_i];
+          for (action in this.actions[method]) {
+            routes.push("" + path + "/" + action + " " + (method.toUpperCase()) + " => " + this.name + "#" + action);
+          }
+        }
+        _ref = this.singular ? [1, descriptions.singular] : [0, descriptions.plural], start = _ref[0], desc = _ref[1];
+        for (i = _j = start, _ref1 = actions.length - 1; start <= _ref1 ? _j <= _ref1 : _j >= _ref1; i = start <= _ref1 ? ++_j : --_j) {
+          if (this.actions[actions[i]]) {
+            routes.push("" + path + desc[i][1] + " " + (desc[i][0].toUpperCase()) + " => " + this.name + "#" + actions[i]);
+          }
+        }
+        return routes;
+      }
+    };
+  };
+
+  addCallback = function(method, action, callback) {
+    var errorMsg, _base;
+    errorMsg = "The " + ([method, action].join(':').replace(/:$/, '')) + " action already exists on the '" + this.name + "' resource. Try removing it first.";
+    if (action) {
+      (_base = this.actions)[method] || (_base[method] = {});
+      if (this.actions[method][action]) {
+        throw errorMsg;
+      }
+      return this.actions[method][action] = callback;
+    } else {
+      if (this.actions[method]) {
+        throw errorMsg;
+      }
+      if (this.singular && method === 'index') {
+        throw "Adding index to '" + this.name + "' isn't possible (singular resource).";
+      }
+      return this.actions[method] = callback;
+    }
+  };
+
+  handleRequest = function(method, url, options, original, optionsHandler) {
+    var action, callback, index, key, matches, path, pathvar, proceed, proceeded, resource, result, urlParts, vars, _i, _len, _ref, _ref1;
+    method = method.toLowerCase();
+    _ref = urlParts = parseUrl(url), path = _ref.path, action = _ref.action;
+    proceeded = false;
+    proceed = function(opts) {
+      proceeded = true;
+      return original(url, optionsHandler(options || {}, opts || {}));
+    };
+    for (key in resources) {
+      resource = resources[key];
+      if (!(matches = path.match(resource.path))) {
+        continue;
+      }
+      if (callback = determineCallback(resource, action, method, matches[matches.length - 2], matches[matches.length - 3])) {
+        vars = {};
+        _ref1 = resource.pathvars;
+        for (index = _i = 0, _len = _ref1.length; _i < _len; index = ++_i) {
+          pathvar = _ref1[index];
+          vars[pathvar] = matches[index + 1];
+        }
+        result = callback(proceed, vars, urlParts);
+      }
+      if (result !== false && !proceeded) {
+        return proceed(result);
+      }
+      return;
+    }
+    return original(url, options);
+  };
+
+  determineCallback = function(resource, action, method, matchAction, matchIdOrAction) {
+    var _ref, _ref1, _ref2, _ref3;
+    switch (method) {
+      case 'get':
+        if ((_ref = resource.actions.get) != null ? _ref[action] : void 0) {
+          return resource.actions.get[action];
+        }
+        switch ((matchIdOrAction ? action : '')) {
+          case '':
+            return (resource.singular ? resource.actions.show : resource.actions.index);
+          case 'new':
+            return resource.actions["new"];
+          case 'edit':
+            return resource.actions.edit;
+          default:
+            if (!matchAction) {
+              return resource.actions.show;
+            }
+        }
+        break;
+      case 'put':
+        return ((_ref1 = resource.actions.put) != null ? _ref1[action] : void 0) || (!matchAction ? resource.actions.update : void 0);
+      case 'post':
+        return ((_ref2 = resource.actions.post) != null ? _ref2[action] : void 0) || (!matchIdOrAction ? resource.actions.create : void 0);
+      case 'delete':
+        return ((_ref3 = resource.actions["delete"]) != null ? _ref3[action] : void 0) || (!matchAction ? resource.actions.destroy : void 0);
+    }
+  };
+
+  this.Resourcy = {
+    removeAll: function() {
+      return resources = {};
+    },
+    handleRequest: handleRequest,
+    noConflict: Resourcy.noConflict || function() {
+      return delete Resourcy;
+    },
+    resources: function(path, actions) {
+      if (actions == null) {
+        actions = {};
+      }
+      return createResource(path).add(actions);
+    },
+    resource: function(path, actions) {
+      if (actions == null) {
+        actions = {};
+      }
+      return createResource(path, true).add(actions);
+    },
+    routes: function() {
+      var path, resource, routes;
+      routes = {};
+      for (path in resources) {
+        resource = resources[path];
+        routes[resource.name] = resource.describe();
+      }
+      return routes;
+    }
+  };
+
+}).call(this);
